769989c48d8a916405d7dabde8975410385bf565
DrawRanger1998 <105395346+DrawRanger1998@users.noreply.github.com>
2022-05-19 02:52:36 -0500

    Update IrisColoresPuntos.R

--- Pregunta-5/IrisColoresPuntos.R
+++ Pregunta-5/IrisColoresPuntos.R
@@ -1,5 +1,1 @@
-#Selccionamos una columna de la tabla iris
-#bg le damos un color a los puntos
-#col le damos color a lobs bordes
-#iri[1:4] escogemos las 4 columnas
 plot(iris[1:4], pch=1, col=c("red","green3","blue")[unclass(iris$Species)])
